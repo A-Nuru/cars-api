dc2939fca842e61da2c53b09fb5ec126377456b9
nurudeen <access2abey@gmail.com>
2023-04-13 17:22:50 +0100

    created GET API call for cars

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-
+const fruits = require('./cars.json')
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 
-
+app.get('/', (req, res) => {
+    res.send("I am a car")
+  })
+  
+  app.get('/cars', (req, res) => {
+    res.send("Hello, cars")
+  })
+  
+  app.get('/cars/:id', (req, res) => {
+    res.send(req.query)
+  })
 
 
 
