db999641a42463722ba3e587ac52d90706289472
nurudeen <access2abey@gmail.com>
2023-04-13 17:34:09 +0100

    updated GET API call for cars

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const cors = require('cors')
-const fruits = require('./cars.json')
+const cars = require('./cars.json')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("I am a car")
+app.get('/cars', (req, res) => {
+    res.send(cars)
   })
   
-  app.get('/cars', (req, res) => {
-    res.send("Hello, cars")
-  })
-  
-  app.get('/cars/:id', (req, res) => {
-    res.send(req.query)
-  })
+app.get('/cars/:name', (req, res) => {
+    const name = req.params.name.toLowerCase()
+    console.log(name)
+
+    const car = cars.find(car => car.name.toLowerCase() === name)
+    console.log(car)
+    if (car === undefined) {
+        res.status(404).send({ error: `car: ${name} not found :(`})
+    }
+    res.send(car)
+})
+
 
 
 
